5287180817ae9921ab19e3cc76dbad24fb41393e
someyahirofumi <hirofumi.someya@rakus-partners.co.jp>
2022-02-14 11:07:41 +0900

    プロジェクト作成

---
+++ src/main/java/com/example/ExIntermedeateApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExIntermedeateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExIntermedeateApplication.class, args);
+	}
+
+}
